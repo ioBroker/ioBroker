e555c40f23ad0ef72592280b207673ff4fa5f61a
hometm <hometm@users.noreply.github.com>
2015-01-30 18:10:02 +0100

    Update install.js

--- install/windows/install.js
+++ install/windows/install.js
@@ -25,7 +25,7 @@ svc.on('alreadyinstalled', function () {
 // Listen for the "start" event and let us know when the
 // process has actually started working.
 svc.on('start', function () {
-  console.log(svc.name + ' started!\nVisit http://127.0.0.1:8080 to configure it.');
+  console.log(svc.name + ' started!\nVisit http://127.0.0.1:8081 to configure it.');
 });
 
 // Install the script as a service.
