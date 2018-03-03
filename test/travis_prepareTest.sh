88c46c0187ecdfbc1f14e8c799171ad71c2873d4
Dominic Griesel <d.griesel@gmx.net>
2018-03-03 08:26:20 +0100

    keep travis tests in root dir

--- test/travis_prepareTest.sh
+++ test/travis_prepareTest.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-cd ../..
-
 sudo chmod -R 777 *
 ps auxww|grep io
 ./iobroker start
+
+npm install request mocha chai --save
+
 sleep 60
 ps auxww|grep io
 date
-cat ../../log/iobroker*.log
-
-cd node_modules/iobroker/
+cat log/iobroker*.log
