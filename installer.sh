b6c6405d15d21bb3e2ba8e737c22035943a59e7c
Al Calzone <d.griesel@gmx.net>
2018-09-02 18:01:28 +0200

    Fix permission problems

--- installer.sh
+++ installer.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-# ensure the directory exists
+# ensure the directory exists and take control of it
 sudo mkdir -p /opt/iobroker
+sudo chown $USER -R /opt/iobroker
 cd /opt/iobroker
+
 # download the installer files and run them
 npm i https://github.com/AlCalzone/ioBroker/tarball/install-v2
 # TODO: GH#48 Make sure we don't need sudo, so we can remove that and --unsafe-perm
 sudo npm i --production --unsafe-perm
 # npm i --production # this is how it should be
+# Because we used sudo, we now need to take control again
+sudo chown $USER -R /opt/iobroker
 
 # If we want to autostart ioBroker with systemd, enable that
 if [ -f /lib/systemd/system/iobroker.service ];
 then
-	sudo systemctl daemon-reload
-	sudo systemctl enable iobroker
-	sudo systemctl start iobroker
+	# We cannot use sudo here because it will fail silently otherwise
+	echo "Enabling autostart..."
+	systemctl daemon-reload
+	systemctl enable iobroker
+	systemctl start iobroker
+	echo "Autostart enabled!"
 fi
