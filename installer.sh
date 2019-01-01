f404fb57558ff2cfce91c8fabd8aa9b2b9b63a0f
Dominic Griesel <d.griesel@gmx.net>
2019-01-01 18:10:18 +0100

    * Install acl if it is not installed * Use setfacl to set permissions on iobroker directory * Print a warning and create flag file if that failed

--- installer.sh
+++ installer.sh
@@ -2,22 +2,22 @@
 
 # Enable colored output
 if test -t 1; then # if terminal
-    ncolors=$(which tput > /dev/null && tput colors) # supports color
-    if test -n "$ncolors" && test $ncolors -ge 8; then
-        termcols=$(tput cols)
-        bold="$(tput bold)"
-        underline="$(tput smul)"
-        standout="$(tput smso)"
-        normal="$(tput sgr0)"
-        black="$(tput setaf 0)"
-        red="$(tput setaf 1)"
-        green="$(tput setaf 2)"
-        yellow="$(tput setaf 3)"
-        blue="$(tput setaf 4)"
-        magenta="$(tput setaf 5)"
-        cyan="$(tput setaf 6)"
-        white="$(tput setaf 7)"
-    fi
+	ncolors=$(which tput > /dev/null && tput colors) # supports color
+	if test -n "$ncolors" && test $ncolors -ge 8; then
+		termcols=$(tput cols)
+		bold="$(tput bold)"
+		underline="$(tput smul)"
+		standout="$(tput smso)"
+		normal="$(tput sgr0)"
+		black="$(tput setaf 0)"
+		red="$(tput setaf 1)"
+		green="$(tput setaf 2)"
+		yellow="$(tput setaf 3)"
+		blue="$(tput setaf 4)"
+		magenta="$(tput setaf 5)"
+		cyan="$(tput setaf 6)"
+		white="$(tput setaf 7)"
+	fi
 fi
 
 HLINE="=========================================================================="
@@ -63,9 +63,21 @@ fi
 
 print_bold "Welcome to the ioBroker installer!" "You might need to enter your password a couple of times."
 
-NUM_STEPS=4
+NUM_STEPS=5
 
-print_step "Creating ioBroker directory" 1 "$NUM_STEPS"
+print_step "Installing prerequisites" 1 "$NUM_STEPS"
+# Test if acl is installed, so we can use setfacl
+dpkg -s acl &> /dev/null
+if [ $? -ne 0 ]; then
+	# Install acl, so we can use setfacl
+	if [ "$IS_ROOT" = true ]; then
+		apt install -y acl
+	else
+		sudo apt install -y acl
+	fi
+fi
+
+print_step "Creating ioBroker directory" 2 "$NUM_STEPS"
 # ensure the directory exists and take control of it
 if [ "$IS_ROOT" = true ]; then
 	mkdir -p /opt/iobroker
@@ -78,7 +90,7 @@ cd /opt/iobroker
 # suppress messages with manual installation steps
 touch AUTOMATED_INSTALLER
 
-print_step "Downloading installation files" 2 "$NUM_STEPS"
+print_step "Downloading installation files" 3 "$NUM_STEPS"
 
 # download the installer files and run them
 # If this script is run as root, we need the --unsafe-perm option
@@ -88,7 +100,7 @@ else
 	npm i iobroker --loglevel error
 fi
 
-print_step "Installing ioBroker" 3 "$NUM_STEPS"
+print_step "Installing ioBroker" 4 "$NUM_STEPS"
 
 # TODO: GH#48 Make sure we don't need sudo/root, so we can remove that and --unsafe-perm
 # For now we need to run the 2nd part of the installation as root
@@ -99,27 +111,35 @@ else
 fi
 # npm i --production # this is how it should be
 
-print_step "Finalizing installation" 4 "$NUM_STEPS"
+print_step "Finalizing installation" 5 "$NUM_STEPS"
 
 # Remove the file we used to suppress messages during installation
 rm AUTOMATED_INSTALLER
 
 # If we want to autostart ioBroker with systemd, enable that
 if [ -f /lib/systemd/system/iobroker.service ]; then
-	echo "Enabling autostart..."
-
 	# systemd executes js-controller as the user "iobroker", 
 	# so we need to give it the ownershop of /opt/iobroker
+	echo "Fixing directory permissions..."
 	if [ "$IS_ROOT" = true ]; then
 		chown iobroker:iobroker -R /opt/iobroker
+		# No need to give special permissions, root has access anyways
 	else
 		sudo chown iobroker:iobroker -R /opt/iobroker
 		# To allow the current user to install adapters via the shell,
 		# We need to give it access rights to the directory aswell
 		sudo usermod -a -G iobroker $USER
-		sudo chmod -R g+w /opt/iobroker
+		# Give the iobroker group write access to all files by setting the default ACL
+		sudo setfacl -Rdm g:iobroker:rwx /opt/iobroker &> /dev/null && sudo setfacl -Rm g:iobroker:rwx /opt/iobroker &> /dev/null
+		if [ $? -ne 0 ]; then
+			# We cannot rely on default permissions on this system
+			echo "${yellow}This system does not support setting default permissions."
+			echo "${yellow}Do not use npm to manually install adapters unless you know what you are doing!"
+			touch DO_NOT_USE_NPM_MANUALLY
+		fi
 	fi
 
+	echo "Enabling autostart..."
 	if [ "$IS_ROOT" = true ]; then
 		systemctl daemon-reload
 		systemctl enable iobroker
