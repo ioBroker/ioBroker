0fdf7ca40e8ed33d3c3d23b7140b70a9b5facdea
Dominic Griesel <d.griesel@gmx.net>
2019-01-13 16:01:21 +0100

    * Create iobroker and iob executables * Support init.d and systemd * Support FreeBSD (rc.d)

--- installer.sh
+++ installer.sh
@@ -3,12 +3,34 @@
 # Increase this version number whenever you update the installer
 INSTALLER_VERSION="2019-01-02" # format YYYY-MM-DD
 
+# Directory where iobroker should be installed
+IOB_DIR="/opt/iobroker"
+CONTROLLER_DIR="$IOB_DIR/node_modules/iobroker.js-controller"
+
+# The user to run ioBroker as
+IOB_USER="iobroker"
+
 # Test if this script is being run as root or not
 # TODO: To resolve #48, running this as root should be prohibited
 if [[ $EUID -eq 0 ]]; then
 	IS_ROOT=true
 else
 	IS_ROOT=false
+fi
+ROOT_GROUP="root"
+# Test which platform this script is being run on
+unamestr=$(uname)
+if [ "$unamestr" = "Linux" ]; then
+	platform="linux"
+elif [ "$unamestr" = "Darwin" ]; then
+	# OSX and Linux are the same in terms of install procedure
+	platform="linux"
+elif [ "$unamestr" = "FreeBSD" ]; then
+	platform="freebsd"
+	ROOT_GROUP="wheel"
+else
+	echo "Unsupported platform!"
+	exit 1
 fi
 
 # Enable colored output
@@ -67,7 +89,18 @@ print_msg() {
 	echo
 }
 
-create_user() {
+set_root_permissions() {
+	file="$1"
+	if [ "$IS_ROOT" = true ]; then
+		chown root:$ROOT_GROUP $file
+		chmod 755 $file
+	else
+		sudo chown root:$ROOT_GROUP $file
+		sudo chmod 755 $file
+	fi
+}
+
+create_user_linux() {
 	username="$1"
 	id "$username" &> /dev/null;
 	if [ $? -ne 0 ]; then
@@ -78,15 +111,61 @@ create_user() {
 			sudo useradd -m -s /usr/sbin/nologin "$username"
 		fi
 	fi
-	# Add the user to all groups we need
-	# and give him passwordless sudo privileges
-	# TODO: replace NOPASSWD:ALL with a list of commands iobroker may execute
+	# Add the user to all groups we need and give him passwordless sudo privileges
+	# Define which commands may be executed as sudo without password
+	# TODO: Do we need others?
+	SUDOERS_CONTENT=$(cat <<- EOF
+		$username	ALL=(ALL)	ALL
+		$username	ALL=(ALL)	NOPASSWD: /usr/bin/shutdown -h now, /usr/bin/halt, /usr/bin/poweroff, /usr/bin/reboot
+		$username	ALL=(ALL)	NOPASSWD: /usr/bin/mount -o nosuid\,nodev\,noexec, /usr/bin/umount
+		$username	ALL=(ALL)	NOPASSWD: /usr/bin/apt-get, /usr/bin/apt, /usr/bin/dpkg, /usr/bin/make
+		$username	ALL=(ALL)	NOPASSWD: /usr/bin/ping, /usr/sbin/ping, /usr/bin/fping, /usr/sbin/fping, /usr/bin/arp-scan
+		$username	ALL=(ALL)	NOPASSWD: /usr/bin/setcap
+		EOF
+		)
+	if [ "$platform" = "linux" ]; then
+		SUDOERS_FILE="/etc/sudoers.d/iobroker"
+	else
+		SUDOERS_FILE="/usr/local/etc/sudoers.d/iobroker"
+	fi
 	if [ "$IS_ROOT" = true ]; then
 		usermod -a -G bluetooth,dialout,gpio,tty "$username"
-		echo "$username ALL=(ALL) NOPASSWD:ALL" | (EDITOR="tee" visudo -f /etc/sudoers.d/iobroker)
+		echo "$SUDOERS_CONTENT" | (EDITOR="tee" visudo -f $SUDOERS_FILE)
 	else
 		sudo usermod -a -G bluetooth,dialout,gpio,tty "$username"
-		echo "$username ALL=(ALL) NOPASSWD:ALL" | (sudo su -c 'EDITOR="tee" visudo -f /etc/sudoers.d/iobroker')
+		echo "$SUDOERS_CONTENT" | (sudo su -c 'EDITOR="tee" visudo -f $SUDOERS_FILE')
+	fi
+}
+create_user_freebsd() {
+	username="$1"
+	id "$username" &> /dev/null;
+	if [ $? -ne 0 ]; then
+		# User does not exist
+		if [ "$IS_ROOT" = true ]; then
+			pw useradd -m -s /usr/sbin/nologin "$username"
+		else
+			sudo pw useradd -m -s /usr/sbin/nologin "$username"
+		fi
+	fi
+	# Add the user to all groups we need and give him passwordless sudo privileges
+	# Define which commands may be executed as sudo without password
+	# TODO: Find out the correct paths on FreeBSD
+	# SUDOERS_CONTENT=$(cat <<- EOF
+	# 	$username	ALL=(ALL)	ALL
+	# 	$username	ALL=(ALL)	NOPASSWD: /usr/bin/shutdown -h now, /usr/bin/halt, /usr/bin/poweroff, /usr/bin/reboot
+	# 	$username	ALL=(ALL)	NOPASSWD: /usr/bin/mount -o nosuid\,nodev\,noexec, /usr/bin/umount
+	# 	$username	ALL=(ALL)	NOPASSWD: /usr/bin/apt-get, /usr/bin/apt, /usr/bin/dpkg, /usr/bin/make
+	# 	$username	ALL=(ALL)	NOPASSWD: /usr/bin/ping, /usr/sbin/ping, /usr/bin/fping, /usr/sbin/fping, /usr/bin/arp-scan
+	# 	$username	ALL=(ALL)	NOPASSWD: /usr/bin/setcap
+	# 	EOF
+	# 	)
+	# SUDOERS_FILE="/usr/local/etc/sudoers.d/iobroker"
+	if [ "$IS_ROOT" = true ]; then
+		pw usermod -a -G bluetooth,dialout,gpio,tty "$username"
+		# echo "$SUDOERS_CONTENT" | (EDITOR="tee" visudo -f $SUDOERS_FILE)
+	else
+		sudo pw usermod -a -G bluetooth,dialout,gpio,tty "$username"
+		# echo "$SUDOERS_CONTENT" | (sudo su -c 'EDITOR="tee" visudo -f $SUDOERS_FILE')
 	fi
 }
 
@@ -97,15 +176,16 @@ install_package() {
 	if [ $? -ne 0 ]; then
 		# Install it
 		if [ "$IS_ROOT" = true ]; then
-			apt install -y package
+			apt install -y $package
 		else
-			sudo apt install -y package
+			sudo apt install -y $package
 		fi
 	fi
 }
 
 print_bold "Welcome to the ioBroker installer!" "Installer version: $INSTALLER_VERSION" "" "You might need to enter your password a couple of times."
 
+export AUTOMATED_INSTALLER="true"
 NUM_STEPS=5
 
 print_step "Installing prerequisites" 1 "$NUM_STEPS"
@@ -122,24 +202,26 @@ install_package "curl"
 install_package "unzip"
 # TODO: Which other packages do we need by default?
 
-print_step "Creating ioBroker directory" 2 "$NUM_STEPS"
+print_step "Creating ioBroker user and directory" 2 "$NUM_STEPS"
 # Ensure the user "iobroker" exists and is in the correct groups
-create_user "iobroker"
-# ensure the directory exists and take control of it
-if [ "$IS_ROOT" = true ]; then
-	mkdir -p /opt/iobroker
-else
-	sudo mkdir -p /opt/iobroker
-	sudo chown $USER:$USER -R /opt/iobroker
+if [ "$platform" = "linux" ]; then
+	create_user_linux $IOB_USER
+else if [ "$platform" = "freebsd" ]; then
+	create_user_freebsd $IOB_USER
 fi
-cd /opt/iobroker
+
+# Ensure the installation directory exists and take control of it
+if [ "$IS_ROOT" = true ]; then
+	mkdir -p $IOB_DIR
+else
+	sudo mkdir -p $IOB_DIR
+	sudo chown $USER:$USER -R $IOB_DIR
+fi
+cd $IOB_DIR
 
 # Log some information about the installer
 echo "Installer version: $INSTALLER_VERSION" >> INSTALLER_INFO.txt
 echo "Installation date $(date +%F)" >> INSTALLER_INFO.txt
-
-# suppress messages with manual installation steps
-touch AUTOMATED_INSTALLER
 
 print_step "Downloading installation files" 3 "$NUM_STEPS"
 
@@ -166,9 +248,35 @@ fi
 
 print_step "Finalizing installation" 5 "$NUM_STEPS"
 
-# Remove the file we used to suppress messages during installation
-rm AUTOMATED_INSTALLER
+# #############################
+# Create "iob" and "iobroker" executables
+if [ "$platform" = "linux" ]; then
+	IOB_EXECUTABLE=$(cat <<- EOF
+		#!/bin/bash
+		node $CONTROLLER_DIR/iobroker.js $1 $2 $3 $4 $5
+		EOF
+		)
+	IOB_BIN_PATH=/usr/bin
+elif [ "$platform" = "freebsd" ] ; then
+	# TODO: Hashbang?
+	IOB_EXECUTABLE=$(cat <<- EOF
+		node $CONTROLLER_DIR/iobroker.js $1 $2 $3 $4 $5
+		EOF
+		)
+	IOB_BIN_PATH=/usr/local/bin
+fi
+if [ "$IS_ROOT" = true ]; then
+	echo $IOB_EXECUTABLE > $IOB_BIN_PATH/iobroker
+	echo $IOB_EXECUTABLE > $IOB_BIN_PATH/iob
+else
+	sudo echo $IOB_EXECUTABLE > $IOB_BIN_PATH/iobroker
+	sudo echo $IOB_EXECUTABLE > $IOB_BIN_PATH/iob
+fi
+set_root_permissions "$IOB_BIN_PATH/iobroker"
+set_root_permissions "$IOB_BIN_PATH/iob"
 
+# #############################
+# Enable autostart
 # From https://unix.stackexchange.com/questions/18209/detect-init-system-using-the-shell/326213
 	# if [[ `/sbin/init --version` =~ upstart ]]; then echo using upstart;
 	# elif [[ `systemctl` =~ -\.mount ]]; then echo using systemd;
@@ -176,7 +284,10 @@ rm AUTOMATED_INSTALLER
 	# else echo cannot tell; fi
 
 # Test which init system is used:
-if [[ `systemctl` =~ -\.mount ]]; then 
+
+if [[ "$platform" = "freebsd" && -d "/usr/local/etc/rc.d" ]]; then
+	INITSYSTEM="rc.d"
+elif [[ `systemctl` =~ -\.mount ]]; then 
 	INITSYSTEM="systemd"
 elif [[ -f /etc/init.d/cron && ! -h /etc/init.d/cron ]]; then
 	INITSYSTEM="init.d"
@@ -186,30 +297,101 @@ fix_dir_permissions() {
 	# When autostart is enabled, we need to fix the permissions so that `iobroker` can access it
 	echo "Fixing directory permissions..."
 	if [ "$IS_ROOT" = true ]; then
-		chown iobroker:iobroker -R /opt/iobroker
+		chown $IOB_USER:$IOB_USER -R $IOB_DIR
 		# No need to give special permissions, root has access anyways
 	else
-		sudo chown iobroker:iobroker -R /opt/iobroker
+		sudo chown $IOB_USER:$IOB_USER -R $IOB_DIR
 		# To allow the current user to install adapters via the shell,
 		# We need to give it access rights to the directory aswell
-		sudo usermod -a -G iobroker $USER
+		sudo usermod -a -G $IOB_USER $USER
 		# Give the iobroker group write access to all files by setting the default ACL
-		sudo setfacl -Rdm g:iobroker:rwx /opt/iobroker &> /dev/null && sudo setfacl -Rm g:iobroker:rwx /opt/iobroker &> /dev/null
+		sudo setfacl -Rdm g:$IOB_USER:rwx $IOB_DIR &> /dev/null && sudo setfacl -Rm g:$IOB_USER:rwx $IOB_DIR &> /dev/null
 		if [ $? -ne 0 ]; then
 			# We cannot rely on default permissions on this system
 			echo "${yellow}This system does not support setting default permissions.${normal}"
 			echo "${yellow}Do not use npm to manually install adapters unless you know what you are doing!${normal}"
 			echo "ACL enabled: false" >> INSTALLER_INFO.txt
+		else
+			echo "ACL enabled: true" >> INSTALLER_INFO.txt
 		fi
 	fi
 }
 
 # Enable autostart
-if [ "$INITSYSTEM" = "systemd" ]; then
+if [ -z ${IOB_FORCE_INITD+x} ] || [ "$INITSYSTEM" = "init.d"]; then
 	fix_dir_permissions
 	echo "Enabling autostart..."
+
+	# Write a script into init.d that automatically detects the correct node executable and runs ioBroker
+	INITD_FILE=$(cat <<- EOF
+		#!/bin/bash
+		### BEGIN INIT INFO
+		# Provides:          iobroker.sh
+		# Required-Start:    \$network \$local_fs \$remote_fs
+		# Required-Stop::    \$network \$local_fs \$remote_fs
+		# Default-Start:     2 3 4 5
+		# Default-Stop:      0 1 6
+		# Short-Description: starts ioBroker
+		# Description:       starts ioBroker
+		### END INIT INFO
+		PIDF=$CONTROLLER_DIR/lib/iobroker.pid
+		NODECMD=\$(which node)
+		RETVAL=0
+
+		start() {
+		  echo -n "Starting ioBroker"
+		  su - $IOB_USER -c "$NODECMD $CONTROLLER_DIR/iobroker.js start"
+		  RETVAL=$?
+		}
+
+		stop() {
+		  echo -n "Stopping ioBroker"
+		  su - $IOB_USER -c "$NODECMD $CONTROLLER_DIR/iobroker.js stop"
+		  RETVAL=$?
+		}
+		case "$1" in
+		start)
+		  start
+		  ;;
+		stop)
+		  stop
+		  ;;
+		restart)
+		  stop
+		  start
+		  ;;
+		*)
+		  echo "Usage: iobroker {start|stop|restart}"
+		  exit 1
+		  ;;
+		esac
+		exit $RETVAL
+		EOF
+	)
+
+	# Create the startup file, give it the correct permissions and start ioBroker
+	SERVICE_FILENAME="/etc/init.d/iobroker.sh"
+	if [ "$IS_ROOT" = true ]; then
+		echo $INITD_FILE > $SERVICE_FILENAME
+		set_root_permissions $SERVICE_FILENAME
+		sh $SERVICE_FILENAME
+	else
+		sudo echo $INITD_FILE > $SERVICE_FILENAME
+		set_root_permissions $SERVICE_FILENAME
+		sudo sh $SERVICE_FILENAME
+	fi
+	# Remember what we did
+	if [ -z ${IOB_FORCE_INITD+x} ]; then
+		echo "Autostart: init.d (forced)" >> INSTALLER_INFO.txt
+	else
+		echo "Autostart: init.d" >> INSTALLER_INFO.txt
+	fi
+elif [ "$INITSYSTEM" = "systemd" ]; then
+	fix_dir_permissions
+	echo "Enabling autostart..."
+
 	# Write an systemd service that automatically detects the correct node executable and runs ioBroker
-	sudo cat <<- EOF > /lib/systemd/system/iobroker.service
+	SYSTEMD_FILE=$(cat <<- EOF
 		[Unit]
 		Description=ioBroker Server
 		Documentation=http://iobroker.net
@@ -217,37 +399,108 @@ if [ "$INITSYSTEM" = "systemd" ]; then
 		
 		[Service]
 		Type=simple
-		User=iobroker
-		ExecStart=/bin/sh -c "$(which node) /opt/iobroker/node_modules/iobroker.js-controller/controller.js"
+		User=$IOB_USER
+		ExecStart=/bin/sh -c "\$(which node) $CONTROLLER_DIR/controller.js"
 		Restart=on-failure
 		
 		[Install]
 		WantedBy=multi-user.tar
 		EOF
+	)
 
+	# Create the startup file and give it the correct permissions
+	SERVICE_FILENAME="/lib/systemd/system/iobroker.service"
 	if [ "$IS_ROOT" = true ]; then
+		echo $SYSTEMD_FILE > $SERVICE_FILENAME
+		chmod 644 $SERVICE_FILENAME
+
 		systemctl daemon-reload
 		systemctl enable iobroker
 		systemctl start iobroker
 	else
+		sudo echo $SYSTEMD_FILE > $SERVICE_FILENAME
+		sudo chown root:root $SERVICE_FILENAME
+		sudo chmod 644 $SERVICE_FILENAME
+
 		sudo systemctl daemon-reload
 		sudo systemctl enable iobroker
 		sudo systemctl start iobroker
 	fi
 	echo "Autostart enabled!"
 	echo "Autostart: systemd" >> INSTALLER_INFO.txt
-elif [ "$INITSYSTEM" = "init.d"]; then
-	fix_dir_permissions
-	# TODO
-	echo "Autostart: init.d" >> INSTALLER_INFO.txt
+
+elif [ "$INITSYSTEM" = "rc.d" ]; then
+	echo "Enabling autostart..."
+
+	# Write an rc.d service that automatically detects the correct node executable and runs ioBroker
+	RCD_FILE=$(cat <<- EOF
+		#!/bin/sh
+		#
+		# PROVIDE: iobroker
+		# REQUIRE: DAEMON
+		# KEYWORD: shutdown
+
+		. /etc/rc.subr
+
+		name="iobroker"
+		rcvar="iobroker_enable"
+
+		load_rc_config $name
+
+		iobroker_enable=${iobroker_enable-"NO"}
+		iobroker_pidfile=${iobroker_pidfile-"$CONTROLLER_DIR/lib/iobroker.pid"}
+
+		PIDF=$CONTROLLER_DIR/lib/iobroker.pid
+		NODECMD=\$(which node)
+
+		iobroker_start ()
+		{
+			su -m iobroker -c "${NODECMD} ${CONTROLLER_DIR}/iobroker.js start"
+		}
+
+		iobroker_stop ()
+		{
+			su -m iobroker -c "${NODECMD} ${CONTROLLER_DIR}/iobroker.js stop"
+		}
+
+		iobroker_status ()
+		{
+			su -m iobroker -c "${NODECMD} ${CONTROLLER_DIR}/iobroker.js status"
+		}
+
+		PATH="${PATH}:/usr/local/bin"
+		pidfile="${iobroker_pidfile}"
+
+		start_cmd=iobroker_start
+		stop_cmd=iobroker_stop
+		status_cmd=iobroker_status
+
+		run_rc_command "$1"
+		EOF
+		)
+	# Create the startup file, give it the correct permissions and start ioBroker
+	SERVICE_FILENAME="/usr/local/etc/rc.d/iobroker"
+	if [ "$IS_ROOT" = true ]; then
+		echo $RCD_FILE > $SERVICE_FILENAME
+	else
+		sudo echo $RCD_FILE > $SERVICE_FILENAME
+	fi
+	set_root_permissions $SERVICE_FILENAME
+
+	# Enable startup and start the service
+	sysrc iobroker_enable=YES
+	service iobroker start
+
 else
 	echo "${yellow}Unsupported init system, cannot enable autostart!${normal}"
 	echo "Autostart: false" >> INSTALLER_INFO.txt
 	# After sudo npm i, this directory now belongs to root. 
 	# Give it back to the current user
 	# TODO: remove this step when GH#48 is resolved
-	sudo chown $USER:$USER -R /opt/iobroker
+	sudo chown $USER:$USER -R $IOB_DIR
 fi
+
+unset AUTOMATED_INSTALLER
 
 IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 print_bold "${green}ioBroker was installed successfully${normal}" "Open http://$IP:8081 in a browser and start configuring!"
