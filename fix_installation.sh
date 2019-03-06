0e45a45ce7346f069c7bcc38aef6c2d7407c009b
dominic.griesel <d.griesel@gmx.net>
2019-03-06 08:23:23 +0100

    Update versions

--- fix_installation.sh
+++ fix_installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Increase this version number whenever you update the fixer
-FIXER_VERSION="2019-03-05" # format YYYY-MM-DD
+FIXER_VERSION="2019-03-06" # format YYYY-MM-DD
 
 # Test if this script is being run as root or not
 if [[ $EUID -eq 0 ]]; then
