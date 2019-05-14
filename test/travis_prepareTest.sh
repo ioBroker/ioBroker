8fd87df1776b4866b984922657de28d7365390b7
Dominic Griesel <d.griesel@gmx.net>
2019-05-14 21:01:44 +0200

    Execute node and npm as `iobroker`

--- test/travis_prepareTest.sh
+++ test/travis_prepareTest.sh
@@ -5,13 +5,13 @@ set -x
 IOB_DIR=$([ -d /opt/iobroker ] && echo "/opt/iobroker" || echo "/usr/local/iobroker")
 cd $IOB_DIR
 
-sudo chown -R travis:travis /home/travis/.npm
-sudo chmod -R 777 /home/travis/.npm
+# sudo chown -R travis:travis /home/travis/.npm
+# sudo chmod -R 777 /home/travis/.npm
 
-npm install request mocha chai --save
+sudo -H -u iobroker "npm install request mocha chai --save"
 
 ps auxww|grep io
-node node_modules/iobroker.js-controller/iobroker.js start
+sudo -H -u iobroker "node node_modules/iobroker.js-controller/iobroker.js start"
 
 sleep 60
 ps auxww|grep io
