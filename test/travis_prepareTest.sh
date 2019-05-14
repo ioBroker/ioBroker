dbdaf8cc4ca9cd8a563e95af51dc389c43dd1a25
Dominic Griesel <d.griesel@gmx.net>
2019-05-14 20:31:36 +0200

    This way?

--- test/travis_prepareTest.sh
+++ test/travis_prepareTest.sh
@@ -5,11 +5,8 @@ set -x
 IOB_DIR=$([ -d /opt/iobroker ] && echo "/opt/iobroker" || echo "/usr/local/iobroker")
 cd $IOB_DIR
 
-# What are the permissions here?
-cat INSTALLER_INFO.txt
-
-id
-ls -la /home/travis/.npm/_cacache/index-v5/**/*
+sudo chown -R travis:travis /home/travis/.npm
+sudo chmod -R 777 /home/travis/.npm
 
 npm install request mocha chai --save
 
