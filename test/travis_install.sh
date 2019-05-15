7a3209463d3edeaa9fc90cd21850882cee7d449e
dominic.griesel <d.griesel@gmx.net>
2019-05-15 14:32:58 +0200

    Wait a minute...

--- test/travis_install.sh
+++ test/travis_install.sh
@@ -10,7 +10,7 @@ export NODE_MINOR=$($NODE -v | cut -d. -f2)
 export NODE_BUILD=$($NODE -v | cut -d. -f3)
 
 # try to install ioBroker and capture the response code to test its behavior
-sudo env "PATH=$PATH" $NPM install --unsafe-perm --prefix "node_modules/iobroker"; export EXIT_CODE=$?
+sudo -H env "PATH=$PATH" $NPM install --unsafe-perm --prefix "node_modules/iobroker"; export EXIT_CODE=$?
 # node version too old (< 8.12), the script should exit with code 2
 if [[ ($NODE_MAJOR -lt 8) || (($NODE_MAJOR -eq 8) && ($NODE_MINOR -lt 12)) ]]
 then
