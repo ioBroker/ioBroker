e11851aa75c9c0da2281abd998962d4de636b896
ArneDoe <35469194+ArneDoe@users.noreply.github.com>
2019-10-18 10:55:58 +0200

    Update libloader.sh

--- libloader.sh
+++ libloader.sh
@@ -6,7 +6,7 @@ LIB_NAME="instlib.sh"
 LIB_URL="https://raw.githubusercontent.com/ArneDoe/ioBroker/$LIB_NAME
 curl -L $LIB_URL > $LIB_NAME
 if test -f ./$LIB_NAME; then source ./$LIB_NAME; else echo "library not found"; exit -2; fi
-echo "library loaded. GOOD."
+echo "library was loaded. GOOD."
 # test one function of the library
-RET=libtestfunction
+RET=$(libtestfunction)
 if [ "$RET" == "ok" ]; then echo "library works. GOOD"; else echo "library does not work. BAD"; fi
