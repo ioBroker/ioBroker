0f48c434687f5930d780929162b8f64704f3d3a9
Dominic Griesel <d.griesel@gmx.net>
2019-10-19 16:33:13 +0200

    Ensure all files in IOB_DIR belong to the correct user

--- travis/travis_testFiles.sh
+++ travis/travis_testFiles.sh
@@ -7,13 +7,25 @@ cd $IOB_DIR
 
 # On linux, the iobroker user must be able to access the files
 TEST_CMD="sudo -u iobroker test"
+IOB_USER="iobroker"
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 	# On OSX, the current one (i.e. travis)
 	TEST_CMD="test"
+	IOB_USER="travis"
 fi
 
 # Test that the relevant files exist and have the correct permissions
+
+# All files in $IOB_DIR must belong to $IOB_USER and be readable
+shopt -s dotglob # include dotfiles in *.* glob
+for file in *.*; do
+	[ $(stat -c %U $file) == "$IOB_USER" ] # has the correct owner
+	$TEST_CMD -r $file # is readable
+done
+
+# The iobroker binary must be a readable and executable file
 $TEST_CMD -f ./iobroker # is a file
 $TEST_CMD -r ./iobroker # is readable
 $TEST_CMD -x ./iobroker # is executable
+
 # add others when necessary
