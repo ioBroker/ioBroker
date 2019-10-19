01f2902aaf9cc174300300af15e3fd950545d091
Dominic Griesel <d.griesel@gmx.net>
2019-10-19 14:45:50 +0200

    nah, we need permission tests on OSX aswell [appveyor skip]

--- travis/travis_testFiles.sh
+++ travis/travis_testFiles.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 IOB_DIR=$([ -d /opt/iobroker ] && echo "/opt/iobroker" || echo "/usr/local/iobroker")
 cd $IOB_DIR
 
+# On linux, the iobroker user must be able to access the files
+TEST_CMD="sudo -u iobroker test"
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+	# On OSX, the current one (i.e. travis)
+	TEST_CMD="test"
+fi
+
 # Test that the relevant files exist and have the correct permissions
-sudo -u iobroker test -r ./iobroker && echo "OK" # readable
-sudo -u iobroker test -x ./iobroker && echo "OK" # executable
+$TEST_CMD -f ./iobroker # is a file
+$TEST_CMD -r ./iobroker # is readable
+$TEST_CMD -x ./iobroker # is executable
 # add others when necessary
