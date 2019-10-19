f84ade293ad6cbc479b35e5bee8a84046585f928
Dominic Griesel <d.griesel@gmx.net>
2019-10-19 09:39:59 +0200

    make test result readable

--- test/travis_testFiles.sh
+++ test/travis_testFiles.sh
@@ -6,6 +6,6 @@ IOB_DIR=$([ -d /opt/iobroker ] && echo "/opt/iobroker" || echo "/usr/local/iobro
 cd $IOB_DIR
 
 # Test that the relevant files exist and have the correct permissions
-sudo -u iobroker test -r ./iobroker # readable
-sudo -u iobroker test -x ./iobroker # executable
+sudo -u iobroker test -r ./iobroker && echo "OK" # readable
+sudo -u iobroker test -x ./iobroker && echo "OK" # executable
 # add others when necessary
