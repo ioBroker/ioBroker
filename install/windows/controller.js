2db30159aca567db9ec396660c714bdffa920a17
dilesoft <dilesoft@gmail.com>
2022-05-17 15:18:27 +0300

    install service

---
+++ install/windows/controller.js
@@ -0,0 +1,2 @@
+'use strict';
+require(__dirname + '/node_modules/iobroker.js-controller/controller.js');
---
+++ install/windows/serviceIoBroker.bat
@@ -0,0 +1,51 @@
+:::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights
+:::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation
+ECHO **************************************
+
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
+ECHO args = "ELEV " >> "%temp%\OEgetPrivileges.vbs"
+ECHO For Each strArg in WScript.Arguments >> "%temp%\OEgetPrivileges.vbs"
+ECHO args = args ^& strArg ^& " "  >> "%temp%\OEgetPrivileges.vbs"
+ECHO Next >> "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+"%SystemRoot%\System32\WScript.exe" "%temp%\OEgetPrivileges.vbs" %*
+exit /B
+
+:gotPrivileges
+if '%1'=='ELEV' shift /1
+setlocal & pushd .
+cd /d %~dp0
+
+::::::::::::::::::::::::::::
+::START
+::::::::::::::::::::::::::::
+if '%1' == 'start' %WINDIR%\system32\net.exe start ioBroker
+if '%1' == 'stop' %WINDIR%\system32\net.exe stop ioBroker
+if '%1' == '' (
+	%WINDIR%\system32\net.exe stop ioBroker
+	%WINDIR%\system32\net.exe start ioBroker
+)
+if '%1' == 'restart' (
+	%WINDIR%\system32\net.exe stop ioBroker
+	%WINDIR%\system32\net.exe start ioBroker
+)
