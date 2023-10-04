d87d999813514af97922972282049b1e66148e7b
Grothesk242 <57298681+Grothesk242@users.noreply.github.com>
2023-10-04 23:39:40 +0200

    Bump version

--- installer_library.sh
+++ installer_library.sh
@@ -1,7 +1,7 @@
 # ------------------------------
 # Increase this version number whenever you update the lib
 # ------------------------------
-LIBRARY_VERSION="2023-08-31" # format YYYY-MM-DD
+LIBRARY_VERSION="2023-10-04" # format YYYY-MM-DD
 
 # ------------------------------
 # Supported and suggested node versions
