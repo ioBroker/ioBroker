f0295baaeac928ca7727d7f17c5f400718fef3c6
Grothesk242 <57298681+Grothesk242@users.noreply.github.com>
2023-10-18 02:40:34 +0200

    Fixes concerning Rpi 4+5
    Check for wayfire setup (Rpi 4+5)

--- diag.sh
+++ diag.sh
@@ -8,16 +8,17 @@ then
         echo "";
         elif [ "$(id -u)" = 0 ];
                 then
-                        echo -e "This script must not be run as root! \nPlease use your standard user!"
-                        exit 1
+                        echo -e "You should not use root directly on your system!\nBetter use your standard user!\n\n";
+                        sleep 5;
 
 fi
 clear;
 echo "*** iob diag is starting up, please wait ***";
 # VARIABLES
 export LC_ALL=C;
-SKRIPTV="2023-10-10";      #version of this script
+SKRIPTV="2023-10-18";      #version of this script
 NODE_MAJOR=18           #this is the recommended major nodejs version for ioBroker, please adjust accordingly if the recommendation changes
+
 HOST=$(hostname)
 NODERECOM=$(iobroker state getValue system.host."$HOST".versions.nodeNewestNext);  #recommended node version
 NPMRECOM=$(iobroker state getValue system.host."$HOST".versions.npmNewestNext);    #recommended npm version
@@ -194,10 +195,10 @@ echo -e "\033[34;107m*** User and Groups ***\033[0m";
         echo "$HOME";
         groups;
 echo "";
+
 echo -e "\033[34;107m*** X-Server-Setup ***\033[0m";
-XORGTEST=$(pgrep -f "Xorg")
-# XORGTEST=$(ps aux | grep -c 'Xorg')
-if [[ "$XORGTEST" -gt 1 ]];
+XORGTEST=$(pgrep -c 'Xorg|wayland|X11|wayfire')
+if [[ "$XORGTEST" -gt 0 ]];
         then
                 echo -e "X-Server: \ttrue"
         else
@@ -238,8 +239,8 @@ echo "";
 echo -e "\033[32mMessages concerning ext4 filesystem in dmesg:\033[0m";
 sudo dmesg -T | grep -i ext4;
 echo "";
-echo -e "\033[32mShow mounted filesystems \(real ones only\):\033[0m";
-findmnt --real;
+echo -e "\033[32mShow mounted filesystems:\033[0m";
+findmnt;
 echo "";
 if [[ -L "/opt/iobroker/backups" ]]; then
   echo "backups directory is linked to a different directory";
@@ -466,7 +467,7 @@ else
     echo -e "Timezone: \t\t$(timedatectl | grep zone | cut -c28-80)";
 fi;
 echo -e "User-ID: \t\t$EUID";
-echo -e "X-Server: \t\t$(if [[ $XORGTEST -gt 1 ]]; then echo "true";else echo "false";fi)";
+echo -e "X-Server: \t\t$(if [[ $XORGTEST -gt 0 ]]; then echo "true";else echo "false";fi)";
 if [ -f "$DOCKER" ]; then
         echo -e "";
 else
