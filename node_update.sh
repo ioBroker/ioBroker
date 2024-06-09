f1f4610aad90ec3e4dc21aa9050cc9b689f44092
Grothesk242 <57298681+Grothesk242@users.noreply.github.com>
2024-06-10 00:15:21 +0200

    Update node_update.sh
    * Added code to remove dfsg-nodejs version
    * Suppressed some error messages

--- node_update.sh
+++ node_update.sh
@@ -319,9 +319,9 @@ if
         then
                 echo "Trying to fix your installation now. Please be patient."
                 # Finding nodesource.gpg or nodesource.key and deleting. Current key is pulled in later.
-                $SUDOX rm "$($SUDOX find / \( -path /proc -o -path /dev -o -path /sys -o -path /lost+found -o -path /mnt \) -prune -false -o -name nodesource.[gk]* -print)";
+                $SUDOX rm "$($SUDOX find / \( -path /proc -o -path /dev -o -path /sys -o -path /lost+found -o -path /mnt \) -prune -false -o -name nodesource.[gk]* -print) 2> /dev/null";
                 # Deleting nodesource.list Will be recreated later.
-                $SUDOX rm /etc/apt/sources.list.d/nodesource.lis*;
+                $SUDOX rm /etc/apt/sources.list.d/nodesource.lis* 2> /dev/null;
         else
                 echo "We are not fixing your installation. Exiting.";
 
@@ -341,7 +341,7 @@ fi;
                                 iob stop &
                                 # sudo pkill ^io;
                         else
-                                iob stop &;
+                                iob stop &
                         fi;
 
                 echo "Waiting for ioBroker to shut down - Give me a minute..."
